870df461872b5510f75df93ebd0a9645f2c36fde
simonequarticelli <quarti87@gmail.com>
2019-05-07 11:24:17 +0200

    aggiunti commenti

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-// - Creare un oggetto che descriva uno studente con le seguenti proprietà: nome, cognome e età.
+// Creare un oggetto che descriva uno studente con le seguenti proprietà: nome, cognome e età.
 // Stampare a schermo attraverso il for in tutte le proprietà.
-// - Creare un array di oggetti di studenti.
+// Creare un array di oggetti di studenti.
 // Ciclare su tutti gli studenti e stampare per ognuno nome e cognome
-// - Dare la possibilità all’utente attraverso 3 prompt
+// Dare la possibilità all’utente attraverso 3 prompt
 // di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
 
 
@@ -10,11 +10,11 @@
 var studente = {
   'nome': 'simone',
   'cognome': 'quarticelli',
-  'età': 32,
+  'eta': 32,
 }
 
 //stampo attraverso il for le proprietà
-for (var field in studente) {
+for (var field in studente) { //<-- field vuol dire campo
   if (studente.hasOwnProperty(field)) {
     console.log(studente[field]);
   }
@@ -25,40 +25,40 @@ var lista_studenti = [
   {
     'nome': 'mario',
     'cognome': 'rossi',
-    'età': 21,
+    'eta': 21,
   },
   {
     'nome': 'giacomo',
     'cognome': 'giallo',
-    'età': 23,
+    'eta': 23,
   },
   {
     'nome': 'eleonora',
     'cognome': 'blu',
-    'età': 29,
+    'eta': 29,
   },
   {
     'nome': 'maria',
     'cognome': 'verde',
-    'età': 19,
+    'eta': 19,
   },
   {
     'nome': 'patrizia',
     'cognome': 'nero',
-    'età': 17,
+    'eta': 17,
   },
 ];
 
 //creo ciclo for per scorrere la lista di studenti e stampare il loro nome e cognome
 for (var i = 0; i < lista_studenti.length; i++) {
-  console.log(lista_studenti[i].nome, lista_studenti[i].cognome );
+  console.log(lista_studenti[i].nome, lista_studenti[i].cognome ); //<-- si possono stampare anche con lo spazio + ' ' +
 }
 
 //creo oggetto vuoto
 var nuovo_studente = {
-  'nome': '',
+  'nome': '', //<-- si possono richiamare anche le variabili create ex nome
   'cognome': '',
-  'età': '',
+  'eta': '',
 };
 
 console.log(nuovo_studente);
@@ -71,7 +71,7 @@ var età = parseInt(prompt('inserisci la tua età ', 'età'));
 //assegno le proprietà
 nuovo_studente.nome = nome;
 nuovo_studente.cognome = cognome;
-nuovo_studente.età = età;
+nuovo_studente.età = eta;
 
 //aggiungo nuovo studente (oggetto) nella lista (array)
 lista_studenti.push(nuovo_studente);
